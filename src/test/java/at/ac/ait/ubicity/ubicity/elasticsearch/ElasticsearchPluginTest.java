2e2c1466b5e68ef6e22e08d8088f4fb78bd95477
Christoph Ruggenthaler <christoph.ruggenthaler@gmail.com>
2014-06-25 13:35:40 +0200

    added env.cfg

---
+++ src/test/java/at/ac/ait/ubicity/ubicity/elasticsearch/ElasticsearchPluginTest.java
@@ -0,0 +1,16 @@
+package at.ac.ait.ubicity.ubicity.elasticsearch;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import at.ac.ait.ubicity.ubicity.elasticsearch.impl.ElasticsearchPluginImpl;
+
+public class ElasticsearchPluginTest {
+
+	@Test
+	@Ignore
+	public void test() {
+		new ElasticsearchPluginImpl();
+	}
+
+}
